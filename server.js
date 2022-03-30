ce795d27a7967400fe59e9e2904217f65d7ab152
Cay Henning <cay@ucf.edu>
2022-03-30 17:40:41 -0400

    Change to express server

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
-const host = '0.0.0.0';
+const PORT = process.env.PORT || 3000;
+const HOST = '127.0.0.1';
 
-app.use(express.static(path.resolve(__dirname, )))
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/dist'))
-})
+app.use(express.static(path.join(__dirname, '/dist')));
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}.${PORT}`);
-}
+});
