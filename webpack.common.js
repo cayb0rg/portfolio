1531bf52a51a29f2cf68e4d9fda08fb1ec2e278a
Cay Henning <cay@ucf.edu>
2022-03-30 17:41:03 -0400

    Update webpack config

---
+++ webpack.common.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      template: 'src/index.html'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ]
+      },
+      {
+          test: /\.mp4$/,
+          use: [
+              {
+                  loader: "file-loader",
+                  options: {
+                      name: "[name].[ext]",
+                      outputPath: "video"
+                  }
+              }
+          ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: "[name].[contenthash].[ext]",
+              output: '/fonts'
+            }
+          }
+        ]
+      },
+    ]
+  }
+};
