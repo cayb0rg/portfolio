d34d06a5016d63d72e2da07299ba1175d6e6645f
Cay Henning <cay@ucf.edu>
2022-03-31 16:36:28 -0400

    Added postinstall script and edited server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const HOST = 'localhost';
 
 app.use(express.static(path.join(__dirname, '/dist')));
 
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
+
 app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}.${PORT}`);
+  console.log(`Running on https://${HOST}:${PORT}`);
 });
