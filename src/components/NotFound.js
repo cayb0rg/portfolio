2ce62956c10e385a23e7834f73633ba0087404a4
Cay Henning <cay@ucf.edu>
2022-04-05 16:18:07 -0400

    Cleanup

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NotFound() {
   return (
     <div className="container">
-      <h1>Not Found</h1>
+      <h1>Uh oh. There's nothing here :(</h1>
     </div>
   )
 }
