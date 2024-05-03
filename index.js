aca7b22ff63f53470e970649938a2939c85407d5
Cay <cay.henning@proton.me>
2024-05-03 19:34:02 -0400

    digital ocean stuff, again

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from './src/components/App';
-
-const root = createRoot(document.getElementById('root'));
-
-root.render(<App/>)
+// i love digital ocean
