9fd6aa3c2567e1f616e243893a7a31fdfe74d631
Cay <cay.henning@proton.me>
2024-06-16 22:55:01 -0400

    Add a few more projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 import GitHubLight from '../assets/images/GitHub-Mark-Light-64px.png';
 import GitHubDark from '../assets/images/GitHub-Mark-64px.png';
 import LinkedInLogo from '../assets/images/LI-In-Bug.png';
+import cat from '../assets/images/cat.png';
 
 export default function About(props) {
 
   return (
     <article className="about-container container">
       <h1>about</h1>
-      <p>I'm a Computer Science student at the University of Central Florida. I gained an interest in programming after playing the game <i>Human Resource Machine</i>, and it's only been downhill from there.</p>
-      <p>When I'm not sitting in front of a computer screen, I like exploring places on my bike and reading.</p>
+      <p>I'm a web developer located in Orlando, FL who likes to make things. I gained an interest in programming after playing the game <i>Human Resource Machine</i>, and it's only been downhill from there. My core skillset lies in front-end but I also have an interest in game development, graphics programming, and emulation.</p>
+      <p>When I'm not sitting in front of a computer screen, I like exploring places on my bike and hanging out with my cat.</p>
+      <img id="cat" src={cat} alt="cat" />
       <section className="about-links">
         <a href="https://github.com/cayb0rg" >
           {props.isDarkMode ? <img alt="GitHub" src={GitHubLight}/>
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,32 +1,72 @@
-import camper640 from '../../assets/images/camper_640.png';
-import camperSVG from '../../assets/images/camper.svg';
-import henning640 from '../../assets/images/henning_640.png';
-import stratagem640 from '../../assets/images/stratagem_640.png';
 import libgen640 from '../../assets/images/libgen_640.png';
 import toDoList640 from '../../assets/images/to_do_list_640.png';
 import ticTacToe640 from '../../assets/images/tic_tac_toe_640.png';
+import xpline_640 from '../../assets/images/xpline_640.png';
+import gradCap_640 from '../../assets/images/gradCap_640.png';
+import busDisplay_640 from '../../assets/images/busDisplay_640.png';
+import marching_cubes_640 from '../../assets/images/marching_cubes_640.png';
+
 
 const projects = [
   {
-    param: "drain-cleaning",
-    name: "henning drain cleaning",
-    categories: ["Web Development", "Graphic Design", "HTML5", "CSS3", "Node.js", "Pug"],
-    year: "2019",
-    description: "A website I made using Node.js, Pug, HTML, and CSS for a client. All graphics were designed and made by yours truly in coordinance with the client's vision.",
-    url: "https://henningdraincleaning.com",
+    param: "grad-cap",
+    name: "graduation cap",
+    categories: ["Arduino", "ESP8266", "C++"],
+    year: "2024",
+    description: "A graduation cap I made for my graduation from University of Central Florida. The cap has a 16x16 LED matrix that displays a scrolling message that changes colors. The message can be changed though an HTTP server, accessed by connecting to the ESP8266's access point. The cap is powered by a USB portable charger.",
     images: [
-      henning640
+      gradCap_640,
     ],
-    github: null,
+    url: null,
+    github: "https://github.com/cayb0rg/grad-cap",
+    nextProject: "slack-bus"
+  },
+  {
+    param: "slack-bus",
+    name: "slack bus bot",
+    categories: ["AWS Lambda", "Python", "Docker", "Arduino", "ESP8266", "ESP32", "C++"],
+    year: "2024",
+    description: "A slack bot I made using AWS Lambda, Python, Docker, and an Arduino for DDL Hackday. The bot would respond to commands in the workspace and send information about bus arrival times and last known locations, which it retrieved from the DoubleMap API. This project also had a hardware component, where I used an Arduino to display a countdown until the next bus arrival on a 16x32 LED matrix.",
+    images: [
+      busDisplay_640,
+    ],
+    url: null,
+    github: "https://github.com/cayb0rg/slack-bus",
+    nextProject: "xpline"
+  },
+  {
+    param: "xpline",
+    name: "xpline",
+    categories: ["Arduino", "ESP8266", "C++"],
+    year: "2023",
+    description: "A one-dimensional LED Strip XP Farm for the Techranger’s Minecraft room, inspired by the Line Wobbler dungeon crawler. Spiders, which are represented by the red dots, will start spawning when movement is detected or when the room is dark. To fight off these spiders, players control a Techranger, represented by a blue dot, with a joystick made from two MPU6050s. The XP farm also has its own HTTP server which players can access online to see their current XP.",
+    images: [
+      xpline_640,
+    ],
+    url: null,
+    github: "https://github.com/cayb0rg/xpline",
+    nextProject: "marching-cubes"
+  },
+  {
+    param: "marching-cubes",
+    name: "marching cubes",
+    categories: ["Java", "LWJGL", "OpenGL"],
+    year: "2024",
+    description: "A marching cubes implementation I made using Java, LWJGL, and OpenGL for a group project on parallelizing procedural content generation techniques. The program generates a 2D surface mesh from 3D data.",
+    images: [
+      marching_cubes_640,
+    ],
+    url: null,
+    github: "https://github.com/rachelMae/ParallelizationOfProceduralContentGeneration/tree/cay-experimental",
     nextProject: "to-do-list"
   },
   {
     param: "to-do-list",
     name: "to do list",
-    categories: ["Web Development", "HTML5", "CSS3", "MongoDB", "Node.js", "Pug"],
-    year: "2019",
-    description: "A simple to do list I made using MongoDB, Node.js, Pug, HTML, and CSS as a way to learn the model-view-controller architecture.",
-    url: "https://stratagem-to-do-list.herokuapp.com",
+    categories: ["MongoDB", "Node.js", "Pug", "Adobe Illustrator", "GSAP","HTML5", "CSS3"],
+    year: "2020",
+    description: "A simple to do list I made using MongoDB, Node.js, Pug, HTML, and CSS as a way to learn the model-view-controller architecture. I designed the homepage in Adobe Illustrator and animated it using GSAP, HTML, and CSS. Implementing the fireflies was a challenging but enlightening experience.",
+    url: "https://todo.greml.ing",
     github: "https://github.com/cayb0rg/to-do-list",
     images: [
       toDoList640
@@ -36,27 +76,13 @@ const projects = [
   {
     param: "libgen-api",
     name: "library genesis search tool",
-    categories: ["Web Development", "React.js", "GSAP", "HTML5", "CSS3"],
+    categories: ["React.js", "GSAP", "HTML5", "CSS3"],
     year: "2020",
-    description: "This project was made using React.js, GSAP, and my Library Genesis API. Users can search for books within Library Genesis using the book's title, author, ISBN, etc.",
-    url: "https://cayb0rg.github.io/libgen_api_search_tool/",
+    description: "This purely front-end tool was made using React.js, GSAP, and my Library Genesis API. Users can search for books within Library Genesis using the book's title, author, ISBN, etc.",
+    url: "https://libgen.greml.ing",
     github: "https://github.com/cayb0rg/libgen_api_search_tool",
     images: [
       libgen640
-    ],
-    nextProject: "camper"
-  },
-  {
-    param: "camper",
-    name: "camper animation",
-    categories: ["Adobe Illustrator", "GSAP", "HTML5", "CSS3"],
-    year: "2020",
-    description: "I designed the camper in Adobe Illustrator and animated it using GSAP, HTML, and CSS. Implementing the fireflies was a challenging but enlightening experience.",
-    url: "https://cayb0rg.github.io/camper-animation/",
-    github: "https://github.com/cayb0rg/camper-animation",
-    images: [
-      camper640,
-      camperSVG
     ],
     nextProject: "tic-tac-toe"
   },
@@ -65,13 +91,13 @@ const projects = [
     name: "tic tac toe",
     categories: ["JavaScript", "HTML5", "CSS3"],
     year: "2019",
-    description: "This project was made using JavaScript, HTML, and CSS. Users can choose to play against the computer or another player locally. The computer has an easy and impossible mode.",
+    description: "This project was made using JavaScript, HTML, and CSS. Users can choose to play against the computer or another player locally. The computer has an easy and impossible mode, the latter of which uses the minimax algorithm to determine the best move.",
     url: "https://cayb0rg.github.io/tic-tac-toe/",
     github: "https://github.com/cayb0rg/tic-tac-toe",
     images: [
       ticTacToe640
     ],
-    nextProject: "drain-cleaning"
+    nextProject: "grad-cap"
   }
 ]
 
